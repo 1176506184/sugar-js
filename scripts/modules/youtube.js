d78126148ffb741a4bf2cf01c0bb8fbb5c94aa72
tangfeifan <1050069968@qq.com>
2023-08-22 14:16:13 +0800

    update

--- scripts/modules/youtube.js
+++ scripts/modules/youtube.js
@@ -1,4 +1,5 @@
 const videoData = []
+const videoIds = []
 
 function getVideo() {
     console.log("开始返回数据")
@@ -6,7 +7,7 @@ function getVideo() {
     chrome.runtime.sendMessage({
         Message: 'youtubeVideo',
         data: videoData,
-        author:document.querySelector('.style-scope.ytd-channel-name')?.innerText
+        author: document.querySelector('.style-scope.ytd-channel-name')?.innerText
     }).then()
 }
 
@@ -29,7 +30,12 @@ function parseVideo(data) {
 
             Object.keys(d).forEach((key) => {
                 if (key === 'videoRenderer') {
-                    videoData.push(d[key]);
+
+                    if(!videoIds.includes(d[key]?.videoId)){
+                        videoIds.push(d[key]?.videoId)
+                        videoData.push(d[key]);
+                    }
+
                 } else {
                     work(d[key]);
                 }
