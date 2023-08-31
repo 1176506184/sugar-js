1b44079e707f90ec6f8f61953322ee9042b76715
tangfeifan <1050069968@qq.com>
2023-08-31 14:56:52 +0800

    结构优化

--- packages/sugar-render/src/index.ts
+++ packages/sugar-render/src/index.ts
@@ -1,6 +1,6 @@
 import {sugarCompiler} from "@sugar/sugar-compiler";
 import {createEffect} from "../../../src/main";
-import patchEx from "./patch";
+import patchEx, {bindAttrAndEvent} from "./patch";
 
 export function sugarRender() {
 
@@ -12,6 +12,7 @@ export function sugarRender() {
             el = document.querySelector(el);
         }
         vm.$el = el
+        vm._vnode = el
         components = vm.components
         render = sugarCompiler(el.outerHTML, components, data);
         updateComponent(vm, el, data);
@@ -22,85 +23,17 @@ export function sugarRender() {
     function updateComponent(vm, el, data) {
         function update(vm, el) {
 
-
-            function patch(vm, oldVNode, vnode, parentElm) {
-                if (!parentElm) {
-                    parentElm = document.body;
-                }
-                if (!oldVNode) {
-                    oldVNode = vm.$el;
-                    oldVNode = new VNode(oldVNode.tagName.toLowerCase(), {}, []);
-                    oldVNode.elm = vm.$el;
-                }
-
-                // 根据vnode创建真实的dom，处理属性、事件、静态文本、创建子元素等
-                const createElm = (vnode, parentElm) => {
-                    const {tag, data = {}, children = []} = vnode || {};
-                    if (tag) {
-                        const elm = document.createElement(tag);
-                        const {attrs = {}, on = {}} = data;
-                        // 处理属性
-                        for (const key in attrs) {
-                            if (Object.hasOwnProperty.call(attrs, key)) {
-                                const value = attrs[key];
-                                elm.setAttribute(key, value);
-                            }
-                        }
-
-                        // 处理监听事件
-                        for (const key in on) {
-                            if (Object.hasOwnProperty.call(on, key)) {
-                                if (on[key].value) {
-                                    const event = vm.data[on[key].value]
-                                    event && elm.addEventListener(key, event)
-                                }
-                            }
-                        }
-                        // 处理子元素
-                        if (children && children.length) {
-                            children.forEach((childVNode) => {
-                                createElm(childVNode, elm);
-                            });
-                        }
-                        vnode.elm = elm;
-                        // 移除文档上的旧节点
-                        parentElm.appendChild(elm);
-                    } else if (vnode.text) {
-
-                        let textNode;
-                        // 处理静态文本
-                        textNode = document.createTextNode(vnode.text);
-                        parentElm.appendChild(textNode);
-                    } else if (children) {
-                        children.forEach((child) => {
-                            createElm(child, parentElm);
-                        })
-                    }
-                };
-
-                createElm(vnode, parentElm);
-                parentElm.removeChild(oldVNode.elm);
-
-            }
-
-
             Object.keys(data).forEach((key) => {
                 vm[key] = data[key]
             })
-
 
             bindT(vm)
 
             createEffect(() => {
                 const vnode = render.call(vm);
-                patchEx(vm, vnode);
-
-                // 将vnode转成真实的DOM元素
-                // patch(vm, vm._vnode, vnode, null);
-
-                // 保存旧的vnode
+                bindAttrAndEvent(vm, vnode)
+                patchEx(vm._vnode, vnode);
                 vm._vnode = vnode;
-
             })
 
         }
@@ -120,7 +53,6 @@ export function sugarRender() {
 
 function bindT(vm) {
 
-    // vm._vnode = emptyNodeAt(vm.$el)
 
     function _c(tag = "div", data = {}, children = []) {
         return createElement(tag, data, children)
@@ -143,20 +75,10 @@ function bindT(vm) {
     }
 
     function _for(fun: Function, data: any) {
-
-        // const vnode: any = new VNode();
-        // vnode.children = []
-        // data.forEach((item, index) => {
-        //     vnode.children.push({
-        //         ...fun(item),
-        //         key: index
-        //     });
-        // })
         let nodes = []
         data.forEach((item, index) => {
             nodes.push({
-                ...fun(item),
-                key: index
+                ...fun(item)
             });
         })
         return nodes;
@@ -193,6 +115,7 @@ class VNode {
     private elm: undefined;
     private context: undefined;
     private text: undefined;
+    private key: undefined;
 
     constructor(tag?, data?, children?) {
         this.tag = tag;
@@ -202,6 +125,7 @@ class VNode {
         this.elm = undefined;
         this.context = undefined;
         this.text = undefined;
+        this.key = data?.attrs?.key
     }
 }
 
--- packages/sugar-render/src/patch.ts
+++ packages/sugar-render/src/patch.ts
@@ -1,34 +1,47 @@
 import {nodeOps, NodeTypes} from "@sugar/sugar-shared"
 
-export default function (vm, newVnode) {
+export function bindAttrAndEvent(vm, vnode) {
+    const {tag, data = {}, children = []} = vnode || {};
+    const {attrs = {}, on = {}} = data;
+    if (vnode.tag) {
+        // 处理监听事件
+        for (const key in on) {
+            if (Object.hasOwnProperty.call(on, key)) {
+                if (on[key].value) {
+                    on[key].value = vm.data[on[key].value]
+                }
+            }
+        }
+        if (vnode.children) {
+            for (let i = 0; i < vnode.children.length; i++) {
+                bindAttrAndEvent(vm, vnode.children[i])
+            }
+        }
 
-    let oldVnode = vm._vnode ? vm._vnode : vm.$el
+    }
+
+}
+
+export default function (oldVnode, newVnode) {
+
 
     if (!oldVnode.elm) {
         oldVnode = emptyNodeAt(oldVnode)
     }
 
-
-    if (newVnode.text !== "" && newVnode.text !== undefined) {
-
-    } else {
-
+    let newDom = newVnode.elm
+    if (!newVnode.elm) {
+        newDom = createElement(newVnode);
     }
 
-    let newDom = createElement(newVnode)
-    // if (isSameNode(oldVnode, newVnode)) {
-    //
-    // } else {
-    //     if (oldVnode.elm && oldVnode.elm.parentNode && newDom) {
-    //         nodeOps.insert(newDom, nodeOps.parentNode(oldVnode.elm), oldVnode.elm)
-    //         nodeOps.remove(oldVnode)
-    //     }
-    // }
 
-
-    if (oldVnode.elm && oldVnode.elm.parentNode && newDom) {
-        nodeOps.insert(newDom, nodeOps.parentNode(oldVnode.elm), oldVnode.elm)
-        nodeOps.remove(oldVnode.elm)
+    if (isSameNode(oldVnode, newVnode)) {
+        patchVnode(newVnode, oldVnode)
+    } else {
+        if (oldVnode.elm && oldVnode.elm.parentNode && newDom) {
+            nodeOps.insert(newDom, nodeOps.parentNode(oldVnode.elm), oldVnode.elm)
+            nodeOps.remove(oldVnode.elm)
+        }
     }
 
     function createElement(vnode) {
@@ -50,7 +63,7 @@ export default function (vm, newVnode) {
             for (const key in on) {
                 if (Object.hasOwnProperty.call(on, key)) {
                     if (on[key].value) {
-                        const event = vm.data[on[key].value]
+                        const event = on[key].value
                         event && domNode.addEventListener(key, event)
                     }
                 }
@@ -71,6 +84,174 @@ export default function (vm, newVnode) {
         return domNode
     }
 
+    function patchVnode(newVnode, oldVnode) {
+
+        if (oldVnode === newVnode) {
+            console.log('同节点，不更新')
+            return
+        }
+
+
+        newVnode.elm = oldVnode.elm
+
+
+        if (newVnode.text) {
+            if (oldVnode.text !== newVnode.text) {
+                oldVnode.elm.nodeValue = newVnode.text
+            }
+        } else {
+
+
+            if (oldVnode.children && oldVnode.children.length) {
+                // console.log('新老节点都有childrens属性', oldVnode.elm, oldVnode, newVnode)
+                updateChildren(oldVnode.elm, oldVnode.children, newVnode.children)
+            } else {
+                oldVnode.elm.innerHTML = ''
+                for (let i = 0; i < newVnode.children.length; i++) {
+
+                    let child = newVnode.children[i]
+                    const newChildDom = child.elm
+                    if (newChildDom) {
+                        oldVnode.elm.appendChild(newChildDom)
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    function clearEmptyVnode(Vnodes) {
+
+        return Vnodes.filter((Vnode) => {
+            return Vnode.tag || Vnode.text === "" || Vnode.text
+        })
+
+    }
+
+    function updateChildren(parentDom, oldCh, newCh) {
+
+
+        oldCh = clearEmptyVnode(oldCh);
+        newCh = clearEmptyVnode(newCh)
+
+        // 旧前
+        let oldPreIndex = 0;
+        // 旧后
+        let oldAftIndex = oldCh.length - 1;
+        // 新前
+        let newPreIndex = 0;
+        // 新后
+        let newAftIndex = newCh.length - 1;
+        // 旧前节点
+        let oldSNode = oldCh[oldPreIndex];
+        // 旧后节点
+        let oldENode = oldCh[oldAftIndex];
+        // 新前节点
+        let newSNode = newCh[newPreIndex];
+        // 新后节点
+        let newENode = newCh[newAftIndex];
+
+        while (oldPreIndex <= oldAftIndex && newPreIndex <= newAftIndex) {
+
+            console.log(oldPreIndex, oldSNode)
+            // 新前与旧前
+            if (!oldSNode || !oldCh[oldPreIndex]) {
+                console.log('旧前为空，旧前向后移动')
+                oldSNode = oldCh[++oldPreIndex]
+            } else if (!oldENode || !oldCh[oldAftIndex]) {
+                console.log('旧后为空，旧后向前移动')
+                oldENode = oldCh[--oldAftIndex]
+            } else if (!newSNode || !newCh[newPreIndex]) {
+                console.log('新前为空，新前向后移动')
+                newSNode = newCh[++newPreIndex]
+            } else if (!newENode || !newCh[newAftIndex]) {
+                console.log('新后为空，新后向前移动')
+                newENode = newCh[--newAftIndex]
+            } else if (isSameNode(newSNode, oldSNode)) {
+                console.log('①新前与旧前', newSNode, oldSNode)
+                patchVnode(newSNode, oldSNode)
+                oldSNode = oldCh[++oldPreIndex]
+                newSNode = newCh[++newPreIndex]
+                // 新前与旧后
+            } else if (isSameNode(newSNode, oldENode)) {
+                console.log('②新前与旧后', newSNode, oldENode)
+                parentDom.insertBefore(oldENode.elm, oldSNode.elm)
+                patchVnode(newSNode, oldENode)
+                oldENode = oldCh[--oldAftIndex]
+                newSNode = newCh[++newPreIndex]
+                // 新后与旧前
+            } else if (isSameNode(newENode, oldSNode)) {
+
+                console.log('③新后与旧前', newENode.elm, oldSNode.elm)
+                parentDom.insertBefore(oldSNode.elm, oldENode.elm.nextSibling)
+                patchVnode(newENode, oldSNode)
+                ++oldPreIndex
+                --newAftIndex
+                // oldSNode = oldCh[++oldPreIndex]
+                // newENode = newCh[--newAftIndex]
+                // 新后与旧后
+            } else if (isSameNode(newENode, oldENode)) {
+
+                console.log('④新后与旧后', newENode, oldENode)
+                patchVnode(newENode, oldENode)
+                --oldAftIndex
+                --newAftIndex
+                // oldENode = oldCh[--oldAftIndex]
+                // newENode = newCh[--newAftIndex]
+
+
+            } else {
+                console.log('⑤未匹配到相同的情况下')
+                let keyMap = {}
+                // 未匹配到相同的情况下
+                for (let i = oldPreIndex; i < oldAftIndex; i++) {
+                    if (oldCh[i]) {
+                        keyMap[oldCh[i].key] = i
+                    }
+                }
+                const target = keyMap[newSNode.key]
+                // 如果存在则移动
+                if (target !== undefined) {
+                    console.log('⑤未匹配到相同的情况下-存在-移动', newSNode.text)
+                    patchVnode(oldCh[target], newSNode)
+                    parentDom.insertBefore(oldCh[target].elm, oldSNode.elm)
+                    oldCh[target] = undefined;
+                } else {
+                    console.log('⑤未匹配到相同的情况下-不存在-添加', newSNode, oldSNode)
+                    // 如不存在则新增
+                    parentDom.insertBefore(newSNode.elm, oldSNode.elm)
+                }
+                newSNode = newCh[++newPreIndex]
+            }
+        }
+
+
+        console.log("剩余旧节点结束", oldSNode, oldPreIndex, oldCh, newCh)
+
+        // 循环结束
+        // ①如果循环结束，新节点还有剩余直接添加
+        if (newPreIndex <= newAftIndex) {
+            console.log(`⑥循环完毕-新节点剩余--new--${newPreIndex}--add--${newAftIndex}`)
+            for (let i = newPreIndex; i <= newAftIndex; i++) {
+                // console.log(parentDom, newCh[i].elm, oldSNode.elm)
+                // console.log(newCh[i], oldSNode)
+                parentDom.insertBefore(newCh[i].elm, oldSNode.elm)
+            }
+        }
+        // ②如果循环结束，旧节点还有剩余直接删除
+        if (oldPreIndex <= oldAftIndex) {
+            console.log('⑥循环完毕-旧节点剩余--old--delete')
+            for (let i = oldPreIndex; i <= oldAftIndex; i++) {
+                if (oldCh[i] && oldCh[i].elm) {
+                    console.log('delete...', oldCh[i].text, 'index:', i)
+                    parentDom.removeChild(oldCh[i].elm)
+                }
+            }
+        }
+
+    }
+
 }
 
 export function emptyNodeAt(elm) {
@@ -89,12 +270,14 @@ class VNode {
     private elm: undefined;
     private context: undefined;
     private text: undefined;
+    private key: undefined
 
     constructor(tag?, data?, children?, elm?) {
         this.tag = tag;
         this.data = data;
         // @ts-ignore
         this.children = children;
+        this.key = data.attrs?.key;
         this.elm = elm;
         this.context = undefined;
         this.text = undefined;
