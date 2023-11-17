559cb62f5448cf8c2796f71438d27f8135a12b3a
tangfeifan <1050069968@qq.com>
2023-11-17 15:40:29 +0800

    update

--- vue/utils/utils.js
+++ vue/utils/utils.js
@@ -1,3 +1,5 @@
+const DIR = process.env.DIR;
+console.log(DIR)
 function guid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -74,11 +76,43 @@ function handleCopyValue(text) {
     }
 }
 
+const getNowDate = () => {
+    var date = new Date();
+    var sign2 = ":";
+    var year = date.getFullYear() // 年
+    var month = date.getMonth() + 1; // 月
+    var day = date.getDate(); // 日
+    var hour = date.getHours(); // 时
+    var minutes = date.getMinutes(); // 分
+    var seconds = date.getSeconds() //秒
+    var weekArr = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期天'];
+    var week = weekArr[date.getDay()];
+    // 给一位数的数据前面加 “0”
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (day >= 0 && day <= 9) {
+        day = "0" + day;
+    }
+    if (hour >= 0 && hour <= 9) {
+        hour = "0" + hour;
+    }
+    if (minutes >= 0 && minutes <= 9) {
+        minutes = "0" + minutes;
+    }
+    if (seconds >= 0 && seconds <= 9) {
+        seconds = "0" + seconds;
+    }
+    return year + "-" + month + "-" + day + " " + hour + sign2 + minutes + sign2 + seconds;
+}
+
 
 export {
     guid,
     queryURLParams,
     GetRequest,
     GetQueryString,
-    handleCopyValue
+    handleCopyValue,
+    getNowDate,
+    DIR
 }
