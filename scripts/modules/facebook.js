0f9b37c934c5b303f2409b730b2356a6debea72e
tangfeifan <1050069968@qq.com>
2024-01-09 08:35:40 +0800

    update

--- scripts/modules/facebook.js
+++ scripts/modules/facebook.js
@@ -928,6 +928,12 @@ async function getFeedUrl(needCollect) {
 
 async function getFeedUrlInTitle(needCollect) {
     let arturl = ''
+    try {
+        FireEvent(needCollect.querySelector('a' + '.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.x1lliihq.x1lku1pv[target="_blank"]'), 'pointerover')
+        await wait(2);
+    } catch (e) {
+
+    }
     arturl = needCollect.querySelector('a' + '.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz.x1heor9g.x1lliihq.x1lku1pv[target="_blank"]')?.href;
     await wait(2);
     let outHrefDom = needCollect.querySelector('a.' + 'x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g x1lliihq x1lku1pv'.replaceAll(' ', '.'));
