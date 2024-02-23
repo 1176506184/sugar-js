c2013117e42b158c47de4d3fbc5d4587163621ee
tangfeifan <1050069968@qq.com>
2024-02-23 14:21:50 +0800

    update

--- scripts/modules/facebook.js
+++ scripts/modules/facebook.js
@@ -720,8 +720,6 @@ async function collectHistory() {
                 }
 
                 if (!posturl || posturl === '#') {
-                    FireEvent(needCollect.querySelector('a' + ".x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g xt0b8zv xo1l8bm".replaceAll(' ', '.')), 'pointerover');
-                    await wait(1);
                     posturl = needCollect.querySelector('a' + ".x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1heor9g xt0b8zv xo1l8bm".replaceAll(' ', '.'))?.getAttribute("href");
                 }
 
