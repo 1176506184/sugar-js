6d8036aea1811fbb501f91d98617c183090bd129
tangfeifan <1050069968@qq.com>
2024-02-26 15:03:52 +0800

    update

--- scripts/modules/facebook.js
+++ scripts/modules/facebook.js
@@ -635,6 +635,11 @@ async function collectHistory() {
                 await wait(2);
             }
 
+            if (needCollect.querySelectorAll(dealClass(`x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f`)).length > 1) {
+                needCollect.querySelectorAll(dealClass(`x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f`))[1].click();
+                await wait(2);
+            }
+
             let tagType = 0;
             let tag = needCollect.querySelector('span' + ".x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h".replaceAll(' ', '.'))?.innerText;
             if (needCollect.querySelector('div' + ".x1yx25j4 x13crsa5 x6x52a7 x1rxj1xn xxpdul3".replaceAll(' ', '.'))?.textContent && (needCollect.querySelector('div' + ".x1yx25j4 x13crsa5 x6x52a7 x1rxj1xn xxpdul3".replaceAll(' ', '.'))?.textContent.length > tag?.length || !tag)) {
