c6f8febabd7b6f4c670c95ebc6634947a12d6329
tangfeifan <1050069968@qq.com>
2024-06-28 11:17:34 +0800

    1.修复ssr的一些问题

--- packages/sugar-render/src/patch.ts
+++ packages/sugar-render/src/patch.ts
@@ -4,7 +4,6 @@ import { bulkComponent } from './component';
 
 export default function patch (vm, newVnode) {
   let oldVnode = vm._vnode;
-  console.log(oldVnode);
   if (!oldVnode.elm) {
     oldVnode = emptyNodeAt(oldVnode);
   }
